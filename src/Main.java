0f3708b08cb2c00e4bbc5d284807a9a680a3b82e
yulianpodruhin <yulianpodruhin@mail.ru>
2023-02-27 14:10:33 +0300

    Lab 3 ex 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Tree tree_1 = new Tree(121,"Дуб");
+        Tree tree_2 = new Tree(2302,"Секвоя",true);
+        Tree tree_3 = new Tree();
+        System.out.println(tree_1);
+        System.out.println(tree_2);
+        System.out.println(tree_3);
     }
 }
